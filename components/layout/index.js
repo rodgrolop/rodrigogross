3d1ddab34a2a693cb030764c34e8dcdad38cc01c
Rodrigo Gross López <rodrigogrosslopez@gmail.com>
2021-01-19 14:39:57 +0100

    Update footer error

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import Header from "./header";
-import Footer from "./Footer";
+import Footer from "./footer";
 import Container from "@material-ui/core/Container";
 
 const Layout = ({ children }) => (
