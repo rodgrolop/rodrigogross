9ef8bbdb45464872bb84617e98be4b32d98e6aec
Rodrigo Gross López <rodrigogrosslopez@gmail.com>
2021-01-20 09:22:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,12 @@ const Home = () => (
       </h1>
 
       <p className={styles.description}>This is a work in progress</p>
+      <iframe
+        src="https://flask.io/BcJKdsEwaHCF?embed=true"
+        style="width: 100%; height: 500px;"
+        frameborder="0"
+        allowfullscreen="allowfullscreen"
+      ></iframe>
     </main>
   </div>
 );
